8832639344c3c4dde2e4a261d7b3dbbfc2e4f316
itzmeowww <winthanasan@gmail.com>
2025-09-13 16:23:58 +0900

    fix

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  client: "@hey-api/client-next",
   input: "../server/openapi.json",
   output: {
     format: "prettier",
     path: "./src/api",
   },
-  types: {
-    enums: "javascript",
-  },
+  plugins: ["@hey-api/client-next"],
 });
--- client/src/hey-api.ts
+++
@@ -1,6 +0,0 @@
-import type { CreateClientConfig } from "@/src/api/client";
-
-export const createClientConfig: CreateClientConfig = (config) => ({
-  ...config,
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-});
